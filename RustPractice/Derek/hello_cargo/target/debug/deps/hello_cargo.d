e297b9c214ac3539180dc98bfc4f8f77e55d20a9
Derek <Sadler_derek@comcast.net>
2023-02-28 22:42:10 -0800

    Add files via upload

---
+++ RustPractice/Derek/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Derek\Desktop\VelotixDataProxy\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\Derek\Desktop\VelotixDataProxy\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ RustPractice/Derek/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Derek\Desktop\VelotixDataProxy\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Derek\Desktop\VelotixDataProxy\hello_cargo\src\main.rs
