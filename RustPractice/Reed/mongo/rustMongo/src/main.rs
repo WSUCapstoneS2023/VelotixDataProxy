24f8713156b4eb54661594c1bf39d702508b5c8f
reed <reed.haves@wsu.edu>
2023-04-17 17:02:15 -0700

    Sync'd Hemendra's wordCount program with file input from MongoDB

--- RustPractice/Reed/mongo/rustMongo/src/main.rs
+++ RustPractice/Reed/mongo/rustMongo/src/main.rs
@@ -1,9 +1,16 @@
+use bson::Document;
 use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
 use std::{env, collections::btree_set::Iter};
 use std::error::Error;
 use tokio;
 use chrono::{TimeZone, Utc};
 use mongodb::bson::doc;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+use std::io::{self, BufRead, BufReader};
+use rayon::prelude::*;
+use futures::stream::{StreamExt, TryStreamExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -34,11 +41,86 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Get the 'movies' collection from the 'sample_mflix' database:
     let movies = client.database("sample_mflix").collection("movies");
 
+    let mut cursor = movies.find(None, None).await?;
+
+    let v: Vec<_> = cursor.try_collect().await?;
+
+
+   
+
+    let mut plotV: Vec<String> = Vec::new();
+
+    for movie in v.iter() {
+        tokenize(movie, &mut plotV)
+    }
+
+    for item in plotV.iter() {
+        println!("Printing item: {}", item);
+    }
+
+
+
    
 
 
     let insert_result = movies.insert_one(new_doc.clone(), None).await?;
     println!("New document ID: {}", insert_result.inserted_id);
 
+    // Map phase
+    let intermediate_results = plotV
+        .par_iter()
+        .map(|line| {
+            let words = line.split_whitespace();
+            let mut word_counts = HashMap::new();
+            for word in words {
+                let count = word_counts.entry(word.to_string()).or_insert(0);
+                *count += 1;
+            }
+            word_counts
+        })
+        .collect::<Vec<HashMap<String, u32>>>();
+
+    // Reduce phase
+    let mut final_results = HashMap::new();
+    for word_counts in intermediate_results {
+        for (word, count) in word_counts {
+            let final_count = final_results.entry(word).or_insert(0);
+            *final_count += count;
+        }
+    }
+    let mut output_file = File::create("output.txt").unwrap();
+    for (word, count) in &final_results {
+        writeln!(output_file, "{}: {}", word, count).unwrap();
+    }
+
+    // Query phase
+    let stdin = io::stdin();
+    loop {
+        println!("Enter a word to get its count (or \"exit\" to quit):");
+        let mut input = String::new();
+        stdin.lock().read_line(&mut input).unwrap();
+        let query_word = input.trim();
+        if query_word == "exit" {
+            break;
+        }
+        match final_results.get(query_word) {
+            Some(count) => println!("Count for {}: {}", query_word, count),
+            None => println!("No count found for {}", query_word),
+        }
+    }
+
+
+
    Ok(())
+}
+
+fn tokenize(movie: &Document, mut plotV: &mut Vec<String>) {
+    let plot = movie.get_str("plot");
+    
+    match plot {
+        Ok(v) => plotV.push(v.to_string()),
+        Err(e) => println!("Error: VAE"),
+    }
+    
+
 }
