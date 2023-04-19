e6e60d0c4b5796114dccc84a047ce565a0450728
Derek <derek.sadler@wsu.edu>
2023-04-19 05:30:15 -0700

    Update main.rs

--- RustPractice/Reed/mongo/rustMongo/src/main.rs
+++ RustPractice/Reed/mongo/rustMongo/src/main.rs
@@ -12,6 +12,15 @@ use std::io::{self, BufRead, BufReader};
 use rayon::prelude::*;
 use futures::stream::{StreamExt, TryStreamExt};
 
+// Used to get the input string
+fn get_input() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to readline");
+    let input = input.trim();
+    println!("You entered {}\n", input);
+    return (&input).to_string();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
    // Load the MongoDB connection string from an environment variable:
@@ -25,37 +34,72 @@ async fn main() -> Result<(), Box<dyn Error>> {
          .await?;
    let client = Client::with_options(options)?;
 
-   // Print the databases in our MongoDB cluster:
-   println!("Databases:");
-   for name in client.list_database_names(None, None).await? {
-      println!("- {}", name);
-   }
+    // Print the databases in our MongoDB cluster:
+    println!("Databases:");
+    for name in client.list_database_names(None, None).await? {
+        println!("- {}", name);
+    }
 
-   let new_doc = doc! {
-    "title": "Parasite",
-    "year": 2020,
-    "plot": "A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.",
-    "released": Utc.ymd(2020, 2, 7).and_hms(0, 0, 0),
-    };
+    // Get IO
+    println!("\nEnter Database to Query");
+    let db = get_input();
 
-    // Get the 'movies' collection from the 'sample_mflix' database:
-    let movies = client.database("sample_mflix").collection("movies");
+    // Get the 'tables' collection from the 'db' database:
+    let collections = client.database(&db).list_collection_names(None).await?;
+    for collection in collections {
+        println!("{}", collection);
+    }
 
-    let mut cursor = movies.find(None, None).await?;
+    // Get the table to query
+    println!("\nEnter Table to Query");
+    let tableName = get_input();
 
+    // get table data and put it to a list
+    let table: mongodb::Collection<Document> = client.database(&db).collection(&tableName);
+
+
+    // get each key
+    let result = table.find_one(None, None).await?;
+    
+    // find the key from the result
+    match result {
+        Some(document) => {
+            for (key, value) in document.iter() {
+                println!("{}", key);
+            }
+        },
+        None => println!("No document found"),
+    }
+    println!("\nEnter Key to Query");
+    let key = get_input();
+
+    // search every value in the table
+    let mut cursor = table.find(None, None).await?;
     let v: Vec<_> = cursor.try_collect().await?;
-
     let mut plotV: Vec<String> = Vec::new();
 
     for movie in v.iter() {
-        tokenize(movie, &mut plotV)
+        tokenize(movie, &mut plotV, &key)
     }
+
+
+    // Below code is hidden by comments since it only inserts for movies and no other tables
+    // in future versions we will be sure to add functionality to insert for any table.
+    /*
     
+        let new_doc = doc! {
+        "title": "Parasite",
+        "year": 2020,
+        "plot": "A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.",
+        "released": Utc.ymd(2020, 2, 7).and_hms(0, 0, 0),
+        };
 
 
-    let insert_result = movies.insert_one(new_doc.clone(), None).await?;
-    println!("New document ID: {}", insert_result.inserted_id);
-
+        
+        let insert_result = table.insert_one(new_doc.clone(), None).await?;
+        println!("New document ID: {}", insert_result.inserted_id);
+    */
+    
     // Map phase
     let intermediate_results = plotV
         .par_iter()
@@ -102,15 +146,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
    Ok(())
 }
 
-fn tokenize(movie: &Document, mut plotV: &mut Vec<String>) {
-    let plot = movie.get_str("plot");
+// Tokenizes database elements and inserts them to a document
+fn tokenize(movie: &Document, mut plotV: &mut Vec<String>, key: &str) {
+    let plot = movie.get_str(key);
 
     match plot {
         Ok(v) => plotV.push(v.to_string()),
-        Err(e) => println!(""),
+        Err(e) => print!(""),
     }
     
 }
+
+//set MONGODB_URI='mongodb+srv://reedhavens:reedhavens@cluster0.jbhu5b9.mongodb.net/test?retryWrites=true&w=majority'
+
+// victor@12
+// Sadler_Derek@comcast.net
+// Derek
+// Sadler
+
+// user sadlerderek
+// password
+// Mr1EAKDg76hZ7k7X
+
 
 //set MONGODB_URI='mongodb+srv://reedhavens:reedhavens@rustquickstart-123ab.mongodb.net/test?retryWrites=true&w=majority'
 
